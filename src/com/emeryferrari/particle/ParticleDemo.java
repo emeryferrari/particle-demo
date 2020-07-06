6d7eee33d8dc19b7fe0e1ff97f2fc1ecba7532a6
Emery Ferrari <rubiquikyt@gmail.com>
2020-07-06 12:58:06 +0900

    Update ParticleDemo.java

--- src/com/emeryferrari/particle/ParticleDemo.java
+++ src/com/emeryferrari/particle/ParticleDemo.java
@@ -4,7 +4,6 @@ import java.awt.*;
 public class ParticleDemo {
 	private int time = 500;
 	private int amount = 50000;
-	private int lifetime = 250;
 	private int counter = 0;
 	private int velocityRandomness = 5;
 	private int[] particleArrayX = new int[100000];
@@ -15,11 +14,10 @@ public class ParticleDemo {
 	private int mouseXInit = 0;
 	private int mouseYInit = 0;
 	private double velocityAmount = 0.25;
-	private int lifetimeRandomness = 5;
 	private double[] lifetimeRandomnessArray = new double[100000];
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Particle Demo");
-		frame.setSize(512, 512);
+		frame.setSize(1024, 1024);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(BorderLayout.CENTER, new ParticleDemo().new Renderer());
 		frame.setVisible(true);
@@ -41,14 +39,11 @@ public class ParticleDemo {
 					lifetimeRandomnessArray[counter+(int)i*amount/time] = (Math.random()*2.0)-1.0;
 				}
 			}
-			for (int i = counter-(lifetime+lifetimeRandomness)*amount/time < 0 ? 0 : counter-(lifetime+lifetimeRandomness)*amount/time; i < counter-1; i++) {
-				System.out.println(1);
-				if (lifetimeArray[i] < lifetime + lifetimeRandomnessArray[i]*lifetimeRandomness) {
-					graphics.drawRect(particleArrayX[i], particleArrayY[i], 5, 5);
-					particleArrayX[i] += velocityArrayX[i]*velocityAmount;
-					particleArrayY[i] += velocityArrayY[i]*velocityAmount;
-					velocityArrayY[i] += 0.5;
-				}
+			for (int i = 0; i < particleArrayX.length; i++) { 
+				graphics.fillRect(particleArrayX[i], particleArrayY[i], 5, 5);
+				particleArrayX[i] += velocityArrayX[i]*velocityAmount;
+				particleArrayY[i] += velocityArrayY[i]*velocityAmount;
+				velocityArrayY[i] += 0.5;
 				lifetimeArray[i]++;
 			}
 			if (counter <= amount) {
